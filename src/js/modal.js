1b6d4673e70df9bee940dfea6621111830af4d24
AntonRoston <rostonanton@gmail.com>
2022-11-02 10:15:49 +0200

    Hero_redact_modal_sliderposition_

--- src/js/modal.js
+++ src/js/modal.js
@@ -14,3 +14,20 @@
     refs.body.classList.toggle("scroll");
   }
 })();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open-2]"),
+    closeModalBtn: document.querySelector("[data-modal-close-2]"),
+    modal: document.querySelector("[data-modal]"),
+    body: document.querySelector("body"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+    refs.body.classList.toggle("scroll");
+  }
+})();
