e3022b2b702395861c155c5d4c63206a81fa89d7
viktoriiayuv <48866430+viktoriiayuv@users.noreply.github.com>
2022-11-02 11:45:03 +0000

    Deploying to gh-pages from @ viktoriiayuv/foodsta-parcel-project@d346e9944b39b39f287aa8b2f0def07220838d3e 🚀

---
+++ index.11bf4de9.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-modal-open-hero]"),modal:document.querySelector("[data-modal]"),body:document.querySelector("body")};function e(){o.modal.classList.toggle("is-hidden"),o.body.classList.toggle("no-scroll")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.11bf4de9.js.map
---
+++ index.907f2d24.js
@@ -0,0 +1,2 @@
+(()=>{const o={openModalBtn:document.querySelector("[data-modal-open-offer]"),modal:document.querySelector("[data-modal]"),body:document.querySelector("body")};function e(){o.modal.classList.toggle("is-hidden"),o.body.classList.toggle("no-scroll")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)})();
+//# sourceMappingURL=index.907f2d24.js.map
